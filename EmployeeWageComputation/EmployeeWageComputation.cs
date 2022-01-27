15cc0a12cdf685ba34668642169b6bf21b661591
vismitap <vismita.prabhu@gmail.com>
2022-01-27 23:57:10 +0530

    [Vismita] Add: UC6 solved

--- EmployeeWageComputation/EmployeeWageComputation.cs
+++ EmployeeWageComputation/EmployeeWageComputation.cs
@@ -9,14 +9,15 @@ namespace EmployeeWageComputation
     internal class EmployeeWageComputation
     {   
         const int PRESENT = 1, ABSENT = 0, PART_TIME=2, WAGE_PER_HOUR=20, FULL_DAY_HOUR=8, PART_TIME_HOUR=4, WORKING_DAYS=20;
+        int days = WORKING_DAYS;
         public void CalEmpWage()
         {
             Random random = new Random();
 
             Console.WriteLine("\n---- Monthly Wage Calculation ----");
-            int dailyWage = 0, totalWage = 0;
+            int dailyWage = 0, totalWage = 0, i = 0, totalHrs=0;
 
-            for(int i = 0; i < WORKING_DAYS; i++)
+            while(days > 0 && totalHrs <= 100)
             {
                 int attdStatus = random.Next(0,3);
 
@@ -25,11 +26,13 @@ namespace EmployeeWageComputation
                     case PRESENT:
                         dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
                         totalWage += dailyWage;
+                        totalHrs += FULL_DAY_HOUR;
                         break;
 
                     case PART_TIME:
                         dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
                         totalWage += dailyWage;
+                        totalHrs += PART_TIME_HOUR;
                         break;
 
                     case ABSENT:
@@ -39,9 +42,11 @@ namespace EmployeeWageComputation
                 }
 
                 Console.WriteLine($"The daily Wage for day {i + 1} is: Rs " + dailyWage);
-
+                i++;
+                days--;
             }
-             Console.WriteLine("\nThe total Monthly wage is: Rs " + totalWage+"\n");
+             Console.WriteLine("\nTotal Working Hours in the month: "+totalHrs);
+             Console.WriteLine("The total Monthly wage is: Rs " + totalWage+"\n");
 
 
         }
