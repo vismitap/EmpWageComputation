98c359982e35f597e7013cd3a6e672bdb9963080
vismitap <vismita.prabhu@gmail.com>
2022-01-27 23:48:33 +0530

    [Vismita] Add: UC5 solved

--- EmployeeWageComputation/EmployeeWageComputation.cs
+++ EmployeeWageComputation/EmployeeWageComputation.cs
@@ -8,16 +8,15 @@ namespace EmployeeWageComputation
 {
     internal class EmployeeWageComputation
     {   
-        const int PRESENT = 1, ABSENT = 0, PART_TIME=2, WAGE_PER_HOUR=20, FULL_DAY_HOUR=8, PART_TIME_HOUR=4;
+        const int PRESENT = 1, ABSENT = 0, PART_TIME=2, WAGE_PER_HOUR=20, FULL_DAY_HOUR=8, PART_TIME_HOUR=4, WORKING_DAYS=20;
         public void CalEmpWage()
         {
             Random random = new Random();
 
-            Console.WriteLine("Enter the number of days you want to calculate the daily wage for: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int dailyWage = 0;
+            Console.WriteLine("\n---- Monthly Wage Calculation ----");
+            int dailyWage = 0, totalWage = 0;
 
-            for(int i = 0; i < num; i++)
+            for(int i = 0; i < WORKING_DAYS; i++)
             {
                 int attdStatus = random.Next(0,3);
 
@@ -25,20 +24,24 @@ namespace EmployeeWageComputation
                 {
                     case PRESENT:
                         dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        totalWage += dailyWage;
                         break;
 
                     case PART_TIME:
                         dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                        totalWage += dailyWage;
                         break;
 
                     case ABSENT:
                         dailyWage = 0;
+                        totalWage += dailyWage;
                         break;
                 }
 
-                Console.WriteLine($"The daily Wage for day {i + 1} is: " + dailyWage);
+                Console.WriteLine($"The daily Wage for day {i + 1} is: Rs " + dailyWage);
 
             }
+             Console.WriteLine("\nThe total Monthly wage is: Rs " + totalWage+"\n");
 
 
         }
